1c056dc9ff64691ebd4b73fb07c24cb1fbde5ecb
Mackenzie Browne <mackbrowne@gmail.com>
2017-10-12 21:24:02 -0400

    remove TOGGLE_BEE_MENU

--- imports/ui/reducers/tests/ui.spec.js
+++ imports/ui/reducers/tests/ui.spec.js
@@ -1,5 +1,5 @@
 import reducer, { DEFAULT_STATE } from "../ui";
-import { TOGGLE_SIDE_BAR, TOGGLE_BEE_MENU } from "../../actionTypes/ui";
+import { TOGGLE_SIDE_BAR } from "../../actionTypes/ui";
 
 describe("settings reducer", () => {
   it("should return the initial state", () => {
@@ -25,16 +25,6 @@ describe("settings reducer", () => {
     const expected = {
       ...DEFAULT_STATE,
       sideBarOpen: !DEFAULT_STATE.sideBarOpen
-    };
-    expect(result).toEqual(expected);
-  });
-
-  it("should handle TOGGLE_BEE_MENU", () => {
-    const mockAction = { type: TOGGLE_BEE_MENU };
-    const result = reducer(DEFAULT_STATE, mockAction);
-    const expected = {
-      ...DEFAULT_STATE,
-      beeMenuOpen: !DEFAULT_STATE.beeMenuOpen
     };
     expect(result).toEqual(expected);
   });
