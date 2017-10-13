72cdd7e98ba4269b44e5a35ac934e452ed71bb15
Mackenzie Browne <mackbrowne@gmail.com>
2017-10-12 21:05:00 -0400

    unit test meteor methods

--- imports/api/bees/tests/methods.spec.js
+++ imports/api/bees/tests/methods.spec.js
@@ -5,13 +5,68 @@ import { Mongo } from "meteor/mongo";
 import { ValidatedMethod } from "meteor/mdg:validated-method";
 //import { _ } from 'meteor/underscore';
 //import { Bees } from '../collection';
-import { fetchAllForUser } from "../methods";
+import { fetchAllForUser, insert, updateName, remove } from "../methods";
 
-describe("methods", function() {
+describe("bee methods", function() {
+  let collection;
+  beforeEach(() => {
+    collection = Mongo.Collection.prototype;
+  });
+
   describe("bees.fetchAllForUser", () => {
     it("runs normally", () => {
       const result = fetchAllForUser.run();
-      expect(Mongo.Collection.prototype.find).toHaveBeenCalled();
+      expect(collection.find).toHaveBeenCalled();
     });
+  });
+
+  describe("bees.insert", () => {
+    let bee;
+    beforeEach(() => {
+      bee = {
+        name: "Bee1",
+        type: "Worker"
+      };
+    });
+
+    it("runs normally", () => {
+      const result = insert.run(bee);
+      expect(collection.insert).toHaveBeenCalled();
+    });
+
+    //TODO throws error
+  });
+
+  describe("bees.updateName", () => {
+    let beeId, newName;
+    beforeEach(() => {
+      beeId = "asdf1234";
+      newName = "newName";
+    });
+
+    it("runs normally", () => {
+      const result = updateName.run({ beeId, newName });
+      expect(collection.findOne).toHaveBeenCalledWith(beeId);
+
+      const updateQuery = { $set: { name: newName } };
+      expect(collection.update).toHaveBeenCalledWith(beeId, updateQuery);
+    });
+
+    //TODO throws error
+  });
+
+  describe("bees.remove", () => {
+    let beeId;
+    beforeEach(() => {
+      beeId = "asdf1234";
+    });
+
+    it("runs normally", () => {
+      const result = remove.run({ beeId });
+      expect(collection.findOne).toHaveBeenCalledWith(beeId);
+      expect(collection.remove).toHaveBeenCalledWith(beeId);
+    });
+
+    //TODO throws error
   });
 });
